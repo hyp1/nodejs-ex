752077e3b1802410a3658478467cf0b5f36bf3bf
robert <robert_schweng@hotmail.com>
2018-08-03 22:09:35 +0200

    DB Change neu DB NEU

--- server.js
+++ server.js
@@ -80,58 +80,8 @@ var initDb = function(callback) {
 */
 
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  /*
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-  */
- res.send('{ pageCount: -1 }');
-});
 
-app.get('/pagecount', function (req, res) {
-  /*
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-  */
- res.send('{ pageCount: -1 }');
-});
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-/*
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-*/
 mongoURL= 'mongodb://userTR5:nmdym2aLFpT70Gqi@172.30.130.83/sampledb';
 console.log(mongoURL,'MONGO');
 
