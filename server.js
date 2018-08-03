5cd8ecac3c71b22fd7947a19c91cdef0d6f9033b
robert <robert_schweng@hotmail.com>
2018-08-03 20:22:02 +0200

    DB Change neu DB1

--- server.js
+++ server.js
@@ -44,7 +44,19 @@ var initDb = function(callback) {
 
   var mongodb = require('mongodb');
   if (mongodb == null) return;
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
 
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
 console.log(mongoURL);
 };
 
