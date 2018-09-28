7839ecb8f419987d645aa7d26688455cb797e10b
robert <robert_schweng@hotmail.com>
2018-09-28 20:11:43 +0200

    Access Control

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.engine('html', require('ejs').renderFile); //console log modules unten!
 app.use('/', express.static('public'), function (req, res) {
   //console.log(req.headers);
   res.set({
-    'Origin': 'https://forum.awri.ch',
+    'Access-Control-Allow-Origin': 'https://forum.awri.ch',
     'ETag': '12345'
   })
   //res.sendFile(express.static('public')+'index.html');
